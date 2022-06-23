34470bccea2a48ca281e29bf258804b738340fe9
Will Hughes <will@ortto.com>
2022-06-23 13:37:02 +1000

    fix quoting/paths

--- modules/magento-ami/scripts/ec2_install/scripts/magento-setup.sh
+++ modules/magento-ami/scripts/ec2_install/scripts/magento-setup.sh
@@ -29,7 +29,7 @@ MAGENTO_MQ_HOST=$(grep 'magento_rabbitmq_host:' ${VARIABLE_TEMP_FILE} | tail -n1
 MAGENTO_MQ_USER=$(grep 'magento_rabbitmq_username:' ${VARIABLE_TEMP_FILE} | tail -n1 | awk '{ print $2}')
 MAGENTO_MQ_PASS=$(grep 'rabbitmq_password:' ${VARIABLE_TEMP_FILE} | tail -n1 | awk '{ print $2}')
 MAGENTO_VARNISH_HOST=$(hostname --all-ip-addresses | awk '{$1=$1;print}')
-BASE_DOMAIN=$(grep 'cloudfront_domain:' /tmp/discovered-vars | tail -n1 | awk '{ print $2}')
+BASE_DOMAIN=$(grep 'cloudfront_domain:' ${VARIABLE_TEMP_FILE} | tail -n1 | awk '{ print $2}')
 MAGENTO_CRYPT_KEY=$(grep 'magento_crypt_key:' ${VARIABLE_TEMP_FILE} | tail -n1 | awk '{ print $2}')
 MAGENTO_FILES_S3=$(grep 'magento_files_s3:' ${VARIABLE_TEMP_FILE} | tail -n1 | awk '{ print $2}')
 MAGENTO_CRYPT_KEY=$(grep 'magento_crypt_key:' ${VARIABLE_TEMP_FILE} | tail -n1 | awk '{ print $2}')
@@ -54,7 +54,7 @@ then
     sudo -u magento mv /var/www/html/magento/app/etc/env.php /mnt/efs/magento/app/etc/
     sudo -u magento mv /var/www/html/magento/app/etc/config.php /mnt/efs/magento/app/etc/
     sudo -u magento mv /var/www/html/magento/pub/static /mnt/efs/magento/pub/
-    sudo -u magento mv /var/www/html/magento/var /mnt/efs/magento/  
+    sudo -u magento mv /var/www/html/magento/var /mnt/efs/magento/
 fi
 
 if [ ! -L "/var/www/html/magento/app/etc/env.php" ]
@@ -85,42 +85,42 @@ if [ ! -f "/mnt/efs/magento/app/etc/env.php" ]
 then
     sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento setup:install \
         --backend-frontname=admin \
-        --db-host=${MAGENTO_DB_HOST} \
+        --db-host="${MAGENTO_DB_HOST}" \
         --db-name=magento \
         --db-user=magento \
-        --db-password=${MAGENTO_DB_PASS} \
+        --db-password="${MAGENTO_DB_PASS}" \
         --search-engine=elasticsearch7 \
-        --elasticsearch-host=https://${MAGENTO_ES_HOST} \
+        --elasticsearch-host=https://"${MAGENTO_ES_HOST}" \
         --elasticsearch-port=443 \
         --elasticsearch-index-prefix=magento2 \
         --elasticsearch-enable-auth=0 \
         --elasticsearch-timeout=15 \
-        --http-cache-hosts=${MAGENTO_VARNISH_HOST}:80 \
+        --http-cache-hosts="${MAGENTO_VARNISH_HOST}":80 \
         --session-save=redis \
-        --session-save-redis-host=${MAGENTO_REDIS_SESSION_HOST} \
+        --session-save-redis-host="${MAGENTO_REDIS_SESSION_HOST}" \
         --session-save-redis-port=6379 \
         --session-save-redis-db=0 \
         --session-save-redis-max-concurrency=20 \
         --cache-backend=redis \
-        --cache-backend-redis-server=${MAGENTO_REDIS_CACHE_HOST} \
+        --cache-backend-redis-server="${MAGENTO_REDIS_CACHE_HOST}" \
         --cache-backend-redis-db=0 \
         --cache-backend-redis-port=6379 \
-        --amqp-host=${MAGENTO_MQ_HOST} \
+        --amqp-host="${MAGENTO_MQ_HOST}" \
         --amqp-port=5671 \
-        --amqp-user=${MAGENTO_MQ_USER} \
-        --amqp-password=${MAGENTO_MQ_PASS} \
+        --amqp-user="${MAGENTO_MQ_USER}" \
+        --amqp-password="${MAGENTO_MQ_PASS}" \
         --amqp-ssl=true \
-        --base-url=https://${BASE_DOMAIN}/ \
-        --base-url-secure=https://${BASE_DOMAIN}/ \
+        --base-url=https://"${BASE_DOMAIN}"/ \
+        --base-url-secure=https://"${BASE_DOMAIN}"/ \
         --use-rewrites=1 \
         --use-secure=1 \
         --use-secure-admin=1 \
-        --key=${MAGENTO_CRYPT_KEY} \
-        --admin-firstname=${MAGENTO_ADMIN_FIRSTNAME} \
-        --admin-lastname=${MAGENTO_ADMIN_LASTNAME} \
-        --admin-email=${MAGENTO_ADMIN_EMAIL} \
-        --admin-user=${MAGENTO_ADMIN_USERNAME} \
-        --admin-password=${MAGENTO_ADMIN_PASS} \
+        --key="${MAGENTO_CRYPT_KEY}" \
+        --admin-firstname="${MAGENTO_ADMIN_FIRSTNAME}" \
+        --admin-lastname="${MAGENTO_ADMIN_LASTNAME}" \
+        --admin-email="${MAGENTO_ADMIN_EMAIL}" \
+        --admin-user="${MAGENTO_ADMIN_USERNAME}" \
+        --admin-password="${MAGENTO_ADMIN_PASS}" \
         --cleanup-database | tee /tmp/magento.install.log
 
     sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento deploy:mode:set developer
@@ -134,7 +134,7 @@ then
 
     sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento config:set web/secure/offloader_header X-Forwarded-Proto
     sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento config:set --scope=default --scope-code=0 system/full_page_cache/caching_application 2
-    sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento config:set trans_email/ident_general/email ${MAGENTO_ADMIN_EMAIL}
+    sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento config:set trans_email/ident_general/email "${MAGENTO_ADMIN_EMAIL}"
 
     sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento config:set system/media_storage_configuration/media_database 0
 
@@ -144,9 +144,9 @@ then
     sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento remote-storage:sync
 
     sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento maintenance:disable
-    
-    sudo aws s3 cp /home/magento/.ssh/id_rsa.pub s3://${MAGENTO_BUCKET}/sync/master.pub
-    sudo aws s3 cp $PRIVATEIPFILE s3://${MAGENTO_BUCKET}/sync/
+
+    sudo aws s3 cp /home/magento/.ssh/id_rsa.pub s3://"${MAGENTO_BUCKET}"/sync/master.pub
+    sudo aws s3 cp $PRIVATEIPFILE s3://"${MAGENTO_BUCKET}"/sync/
 
     sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento cron:install
 
@@ -156,7 +156,7 @@ then
     sudo -u magento echo "*/2 * * * * /bin/bash /home/magento/sync.sh" | sudo -u magento tee -a /tmp/tmpcron
     sudo -u magento crontab /tmp/tmpcron
 else
-    sudo -u magento aws s3 cp s3://${MAGENTO_BUCKET}/sync/master.pub /home/magento/master.pub
+    sudo -u magento aws s3 cp s3://"${MAGENTO_BUCKET}"/sync/master.pub /home/magento/master.pub
     sudo -u magento cat /home/magento/master.pub >> /home/magento/.ssh/authorized_keys
     sudo chmod 600 /home/magento/.ssh/authorized_keys
     sudo chown magento. /home/magento/.ssh/authorized_keys
@@ -164,7 +164,7 @@ fi
 
 sudo -u magento php -d memory_limit=-1 /var/www/html/magento/bin/magento cache:flush
 
-echo flushall >/dev/tcp/${MAGENTO_REDIS_CACHE_HOST}/6379
+echo flushall >/dev/tcp/"${MAGENTO_REDIS_CACHE_HOST}"/6379
 
 sudo rm /etc/sudoers.d/91-magento
 
