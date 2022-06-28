918d05017fac3944723bea82621961c285c76455
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-06-28 11:04:28 +1000

    Apply suggestions from code review

--- modules/magento-ami/scripts/ec2_install/scripts/install_mage_credentials.py
+++ modules/magento-ami/scripts/ec2_install/scripts/install_mage_credentials.py
@@ -5,8 +5,7 @@ import os
 import boto3
 import json
 
-REGION = os.environ.get('REGION') if os.environ.get(
-    'REGION') else 'ap-southeast-2'
+REGION = os.environ.get('REGION',  'ap-southeast-2')
 
 session = boto3.Session(region_name=REGION)
 ssm = session.client('ssm')
@@ -19,23 +18,26 @@ with open('/tmp/ec2_install/scripts/ssm.json') as ssm_file:
 
 try:
     mage_composer_username = ssm.get_parameter(
-        Name=f"{ssm_path_prefix}mage_composer_username", WithDecryption=True)
+       Name=f"{ssm_path_prefix}mage_composer_username", 
+       WithDecryption=True
+     )
 except Exception as e:
-    print(
-        f"mage_composer_username: NotFound {ssm_path_prefix}mage_composer_username", e)
+    print(f"mage_composer_username: NotFound {ssm_path_prefix}mage_composer_username: {e}")
 
 try:
     mage_composer_password = ssm.get_parameter(
-        Name=f"{ssm_path_prefix}mage_composer_password", WithDecryption=True)
+        Name=f"{ssm_path_prefix}mage_composer_password", 
+        WithDecryption=True
+    )
 except Exception as e:
-    print("mage_composer_password: NotFound", e)
+    print(f"mage_composer_password: NotFound: {e}")
 
 
 auth_json = {
     'http-basic': {
         'repo.magento.com': {
             'username': mage_composer_username['Parameter']['Value'],
-            'password': mage_composer_password['Parameter']['Value']
+            'password': mage_composer_password['Parameter']['Value'],
         }
     }
 }
