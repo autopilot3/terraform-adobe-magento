0701d2db8a57121170763b86696f62af0c0a2121
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-06-28 12:45:24 +1000

    fix termination script

--- modules/varnish-ami/scripts/local/cleanup-instances.sh
+++ modules/varnish-ami/scripts/local/cleanup-instances.sh
@@ -2,5 +2,5 @@
 instances=$(aws ec2 describe-instances --region "$AWS_REGION" --query 'Reservations[].Instances[].InstanceId' --filters "Name=tag:Name,Values=$INSTANCE_TAG_NAME" --output text)
 
 if [ -n "$instances" ]; then
-    aws ec2 terminate-instances --region "$AWS_REGION" --instance-ids "$instances"
+    aws ec2 terminate-instances --region "$AWS_REGION" --instance-ids $(echo $instances | sed 's/\s/\n/g')
 fi
