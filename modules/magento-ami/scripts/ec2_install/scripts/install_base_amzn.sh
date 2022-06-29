91d54dc50a5a31dcc5f32673ed25293e4a5b5b00
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-06-29 15:30:20 +1000

    start in the right folder and print out vars on login

--- modules/magento-ami/scripts/ec2_install/scripts/install_base_amzn.sh
+++ modules/magento-ami/scripts/ec2_install/scripts/install_base_amzn.sh
@@ -6,3 +6,6 @@ sudo yum -y update
 
 sudo yum install -y curl gnupg2 gnupg ca-certificates  \
     wget unzip lsof strace python3-pip git jq binutils telnet rsync
+
+echo "cd /var/www/html/magento/" | sudo tee -a ~magento/.bashrc
+echo "cat /tmp/discovered-vars" | sudo tee -a ~magento/.bashrc
