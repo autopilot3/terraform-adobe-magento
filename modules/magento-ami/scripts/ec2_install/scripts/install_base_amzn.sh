2ab397d3fe664c0611ea9956adc03b70319cbf2c
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-06-29 15:42:18 +1000

    add magento bin to path

--- modules/magento-ami/scripts/ec2_install/scripts/install_base_amzn.sh
+++ modules/magento-ami/scripts/ec2_install/scripts/install_base_amzn.sh
@@ -8,6 +8,9 @@ sudo yum install -y curl gnupg2 gnupg ca-certificates  \
     wget unzip lsof strace python3-pip git jq binutils telnet rsync
 
 echo "cd /var/www/html/magento/" | sudo tee -a ~magento/.bashrc
+
 echo "cat /tmp/discovered-vars" | sudo tee -a ~magento/.bashrc
+
+echo "PATH=\$PATH:/var/www/html/magento/bin" | sudo tee -a ~magento/.bashrc
 
 echo "magento ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/90-magento-passwordless-sudo
